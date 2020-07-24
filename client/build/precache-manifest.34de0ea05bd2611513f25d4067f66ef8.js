f40baec67b25c89f4077ed0cdf41ee1b60d68cb6
dathanwong <dwong5@villanova.edu>
2020-07-24 11:19:59 -0700

    Created production version

---
+++ client/build/precache-manifest.34de0ea05bd2611513f25d4067f66ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3fe5693fbfa61161caab957b13ef32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feb9198567a77d9c4549",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b2a312e177330927065",
+    "url": "/static/js/2.bf12b6a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf12b6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb9198567a77d9c4549",
+    "url": "/static/js/main.a0a2df63.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
